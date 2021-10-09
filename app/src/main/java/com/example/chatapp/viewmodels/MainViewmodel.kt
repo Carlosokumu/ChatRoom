56a2df9c58f4d828e968cb4bba11406f1b54277c
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-09 10:26:22 +0300

    Viewmodel build up

---
+++ app/src/main/java/com/example/chatapp/viewmodels/MainViewmodel.kt
@@ -0,0 +1,7 @@
+package com.example.chatapp.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.chatapp.remote.Respository
+
+class MainViewmodel(val respository: Respository):ViewModel() {
+}
