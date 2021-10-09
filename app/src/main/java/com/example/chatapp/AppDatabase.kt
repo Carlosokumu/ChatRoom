a9c55046301d106edc9c877af29d5c1547357f1e
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-09 09:57:09 +0300

    Building Room up

--- app/src/main/java/com/example/chatapp/AppDatabase.kt
+++ app/src/main/java/com/example/chatapp/AppDatabase.kt
@@ -1,7 +1,14 @@
 package com.example.chatapp
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.chatapp.local.Daos.PartnerDao
+import com.example.chatapp.local.Daos.UserDao
+import com.example.chatapp.local.Entities.PartnerInfo
+import com.example.chatapp.local.Entities.UserInfo
 
+@Database(entities = [UserInfo::class,PartnerInfo::class],exportSchema = false,version = 1)
 abstract class AppDatabase:RoomDatabase() {
-
+    abstract fun userDao(): UserDao
+    abstract fun PartnerDao(): PartnerDao
 }
