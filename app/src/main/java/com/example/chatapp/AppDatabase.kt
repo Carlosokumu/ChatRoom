19a96df7d486c6021fec040571210bf09d838993
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-09 09:47:41 +0300

    Started room build up

---
+++ app/src/main/java/com/example/chatapp/AppDatabase.kt
@@ -0,0 +1,7 @@
+package com.example.chatapp
+
+import androidx.room.RoomDatabase
+
+abstract class AppDatabase:RoomDatabase() {
+
+}
---
+++ app/src/main/java/com/example/chatapp/local/Daos/UserDao.kt
@@ -0,0 +1,4 @@
+package com.example.chatapp.local.Daos
+
+class UserDao {
+}
