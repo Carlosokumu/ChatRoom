0f2996933a560f6bd475acaf9e822fc9b6ccfe4c
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-20 15:53:53 +0300

    View Flipper set up

--- app/src/main/java/com/example/chatapp/auth.kt
+++ app/src/main/java/com/example/chatapp/auth.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.daimajia.androidanimations.library.Techniques
 import com.daimajia.androidanimations.library.YoYo
+import com.example.chatapp.Utils.CustomView
 import com.hbb20.CountryCodePicker
 import kotlinx.android.synthetic.main.activity_chat_room.*
 import timber.log.Timber
@@ -16,12 +18,16 @@ import timber.log.Timber
 class auth : Fragment(), CountryCodePicker.OnCountryChangeListener {
     private lateinit var ccp: CountryCodePicker
     private lateinit var mobileNumber: EditText
+    private lateinit var otpBtn: Button
     private lateinit var fullNumber: String
     private lateinit var countrycode: String
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v=inflater.inflate(R.layout.fragment_auth,container,false)
-        ccp=v.findViewById(R.id.ccp)
-        mobileNumber=v.findViewById(R.id.mobile_number)
+        val customView=v.findViewById<CustomView>(R.id.customView)
+        val phone=customView.getChildAt(0)
+        ccp=phone.findViewById(R.id.ccp)
+        mobileNumber=phone.findViewById(R.id.mobile_number)
+        otpBtn=v.findViewById(R.id.customView)
         ccp.setOnCountryChangeListener(this)
         return v
     }
