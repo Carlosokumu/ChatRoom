e90ac97a30ddcfa6b98bb36df59819baa9ce8335
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-11-06 15:49:44 +0300

    Chat page init

--- app/src/main/java/com/example/chatapp/ChatPage.kt
+++ app/src/main/java/com/example/chatapp/ChatPage.kt
@@ -5,15 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.chatapp.databinding.FragmentAuthBinding
+import com.example.chatapp.databinding.FragmentChatpageBinding
 
 
-class chatpage : Fragment() {
+class ChatPage : BindingFragment<FragmentChatpageBinding>() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chatpage, container, false)
+        
+        return binding.root
     }
+
+    override val layoutResId: Int
+        get() = R.layout.fragment_chatpage
 
 
 }
