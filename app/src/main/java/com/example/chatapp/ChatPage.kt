05d9dbaf7024814d2f2cda0212337c1052f5c1e1
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-11-08 09:15:28 +0300

    Binding Initialization

--- app/src/main/java/com/example/chatapp/ChatPage.kt
+++ app/src/main/java/com/example/chatapp/ChatPage.kt
@@ -8,10 +8,14 @@ import com.example.chatapp.databinding.FragmentChatpageBinding
 
 
 class ChatPage : BindingFragment<FragmentChatpageBinding>() {
-
+    private val mainPagerAdapter: FragmentAdapter by lazy {
+        FragmentAdapter(requireActivity().supportFragmentManager)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding.toolbar.inflateMenu(R.menu.top_menu)
+        binding.adapter = mainPagerAdapter
+        binding.tabs.setupWithViewPager(binding.viewpager)
         return binding.root
     }
 
