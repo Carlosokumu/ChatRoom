3e6e4393dded02fd359d9870b7af72ae42ac489a
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-11-08 23:13:47 +0300

    Build Pop Up menu

--- app/src/main/java/com/example/chatapp/ChatPage.kt
+++ app/src/main/java/com/example/chatapp/ChatPage.kt
@@ -1,17 +1,29 @@
 package com.example.chatapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.*
+import androidx.core.content.ContextCompat
 import com.example.chatapp.databinding.FragmentChatpageBinding
+import com.skydoves.powermenu.MenuAnimation
+import com.skydoves.powermenu.OnMenuItemClickListener
+import com.skydoves.powermenu.PowerMenu
+import com.skydoves.powermenu.PowerMenuItem
 
 
-class ChatPage : BindingFragment<FragmentChatpageBinding>() {
+class ChatPage : BindingFragment<FragmentChatpageBinding>(),
+    OnMenuItemClickListener<PowerMenuItem> {
     private val mainPagerAdapter: FragmentAdapter by lazy {
         FragmentAdapter(requireActivity().supportFragmentManager)
     }
     private lateinit var settings: MenuItem
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding.adapter =mainPagerAdapter
         binding.tabs.setupWithViewPager(binding.viewpager)
         return binding.root
@@ -23,17 +35,42 @@ class ChatPage : BindingFragment<FragmentChatpageBinding>() {
         get() = R.color.white
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-       inflater.inflate(R.menu.top_menu,menu)
+       inflater.inflate(R.menu.top_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val list= mutableListOf(
+            PowerMenuItem("New Group", false), PowerMenuItem("Settings", false)
+        )
         when(item.itemId){
-            R.id.action_settings ->{
-                
+            R.id.action_settings -> {
+                val popMenu = PowerMenu.Builder(requireContext())
+                    .addItemList(list)
+                    .setAnimation(MenuAnimation.SHOWUP_TOP_LEFT)
+                    .setMenuRadius(10f)
+                    .setMenuShadow(10f)
+                    .setTextColor(ContextCompat.getColor(requireContext(), R.color.beige))
+                    .setTextGravity(Gravity.CENTER)
+                    .setTextTypeface(Typeface.create("sans-serif-medium", Typeface.BOLD))
+                    .setSelectedTextColor(Color.WHITE)
+                    .setMenuColor(Color.WHITE)
+                    .setSelectedMenuColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.colorPrimary
+                        )
+                    )
+                    .setOnMenuItemClickListener(this)
+                    .build()
+                popMenu.showAsDropDown(requireView())
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onItemClick(position: Int, item: PowerMenuItem?) {
+
     }
 
 
