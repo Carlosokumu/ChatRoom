e7a52e86fe63a6c364a5a09cf92bb90930095bb7
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-11-08 07:44:55 +0300

    Binding:"Status Fragment"

--- app/src/main/java/com/example/chatapp/StatusFragment.kt
+++ app/src/main/java/com/example/chatapp/StatusFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.chatapp.databinding.FragmentStatusBinding
 
-class StatusFragment: Fragment() {
+class StatusFragment: BindingFragment<FragmentStatusBinding>() {
+    override val layoutResId: Int
+        get() = R.layout.fragment_status
+    override val color: Int
+        get() =R.color.white
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val v = LayoutInflater.from(container?.context).inflate(R.layout.fragment_chatlist, container)
-        return v
-    }
+
 }
