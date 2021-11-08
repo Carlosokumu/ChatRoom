35107bf719fe5c680b5cc5590979f3e4871f5d36
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-11-08 07:16:39 +0300

    Designed recyclerview layout

---
+++ app/src/main/java/com/example/chatapp/FragmentAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.chatapp
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+
+class CameraFragmentAdapter(fragmentManager: FragmentManager):
+        FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
+    private val fragmentss = listOf(ChatListFragment(),StatusFragment())
+    private val titles = listOf("Chats", "Status")
+    override fun getCount(): Int = fragmentss.size
+
+    override fun getItem(position: Int): Fragment = fragmentss[position] as Fragment
+    override fun getPageTitle(position: Int): CharSequence? = titles[position]
+}
