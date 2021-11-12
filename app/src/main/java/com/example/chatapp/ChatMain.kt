51183b6fb995b88dfec539699bc5d294bfab66ff
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-11-12 11:49:46 +0300

    ChatPage Fragment

---
+++ app/src/main/java/com/example/chatapp/ChatMain.kt
@@ -0,0 +1,25 @@
+package com.example.chatapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.chatapp.databinding.FragmentChatMainBinding
+
+
+class ChatMain : BindingFragment<FragmentChatMainBinding>() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return binding.root
+    }
+
+    override val layoutResId: Int
+        get() = R.layout.fragment_chat_main
+    override val color: Int
+        get() = R.color.anotherWhats
+
+
+}
